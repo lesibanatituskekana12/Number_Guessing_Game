78ea886214169e388744996272043daca03fbef4
Lesibana Titus Kekana <titusmahlatsekekana@gmail.com>
2025-01-10 13:33:18 +0200

    feat: Add loop for multiple guesses

--- number_guess.sh
+++ number_guess.sh
@@ -3,4 +3,17 @@
 secret_number=$(( RANDOM % 1000 + 1 ))
 
 echo "Guess the secret number between 1 and 1000:"
-read user_guess
+attempts=0
+while true; do
+    read user_guess
+    ((attempts++))
+
+    if [[ $user_guess -eq $secret_number ]]; then
+        echo "You guessed it in $attempts tries. The secret number was $secret_number. Nice job!"
+        break
+    elif [[ $user_guess -gt $secret_number ]]; then
+        echo "It's lower than that, guess again:"
+    else
+        echo "It's higher than that, guess again:"
+    fi
+done
